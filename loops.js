6c0aef825c0db6f12d52e3e95950c4f31981a159
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-06 11:18:13 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+var forLoop = (array) => {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array[i] = "I am 1 strange loop."
+    } else {
+      array[i] = `I am ${i} strange loops.`
+    }
+  }
+  return
+}
+
+var whileLoop = (n) => {
+  while(n >= 0) {
+    console.log(n)
+    n--
+  }
+  return "done"
+}
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+var doWhileLoop = (num) => {
+  do {
+    console.log("I run once regardless.")
+  } while {
+    incrementVariable() < num
+  }
+}
